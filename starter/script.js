9f525f7015daf5f60660c693262368a0e3ef2dcf
hasan <mdhasanhk.98@gmail.com>
2022-11-16 23:45:35 +0600

    first commit

---
+++ starter/script.js
@@ -0,0 +1,4 @@
+'use strict';
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Love you all';
+document.querySelector('.guess').value = 50;
