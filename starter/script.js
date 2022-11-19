92283ecaa2307bd4ee619b6ad20b864162d64e9e
hasan <mdhasanhk.98@gmail.com>
2022-11-19 10:53:05 +0600

    game done

--- starter/script.js
+++ starter/script.js
@@ -4,19 +4,30 @@ console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Love you all';
 document.querySelector('.guess').value = 50;
 */
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
-console.log(secretNumber);
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  //when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '🚫No number!';
+
+    //when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉Correct number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    //when guess to high
   } else if (guess > secretNumber) {
     if (score > 1) {
       score--;
@@ -25,6 +36,8 @@ document.querySelector('.check').addEventListener('click', function () {
     } else {
       document.querySelector('.message').textContent = '💥you loose the game!';
     }
+
+    //when guess to low
   } else if (guess < secretNumber) {
     if (score > 1) {
       score--;
@@ -34,4 +47,16 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '💥you loose the game!';
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  let score = 20;
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').textContent = '?';
+
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('body').style.backgroundColor = '#222';
 });
