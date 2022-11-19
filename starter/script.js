808bb0c3b2032cdcbe2abd4918be470fcecdad88
hasan <mdhasanhk.98@gmail.com>
2022-11-19 16:31:24 +0600

    complete project

--- starter/script.js
+++ starter/script.js
@@ -6,7 +6,11 @@ document.querySelector('.guess').value = 50;
 */
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
-let highscore = 0;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -14,45 +18,59 @@ document.querySelector('.check').addEventListener('click', function () {
 
   //when there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = '🚫No number!';
-
-    //when player wins
-  } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉Correct number!';
+    // document.querySelector('.message').textContent = '🚫No number!';
+    displayMessage('🚫No number!');
+  } //when player wins
+  else if (guess === secretNumber) {
+    // document.querySelector('.message').textContent = '🎉Correct number!';
+    displayMessage('🎉Correct number!');
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = secretNumber;
-    if (score > highscore) {
-      highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
-    }
-
-    //when guess to high
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      score--;
-      document.querySelector('.score').textContent = score;
-      document.querySelector('.message').textContent = '📈Too high!';
-    } else {
-      document.querySelector('.message').textContent = '💥you loose the game!';
-    }
-
-    //when guess to low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      score--;
-      document.querySelector('.score').textContent = score;
-      document.querySelector('.message').textContent = '📈Too low!';
-    } else {
-      document.querySelector('.message').textContent = '💥you loose the game!';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highScore').textContent = highScore;
     }
   }
+  //when player not guess
+  else if (guess !== secretNumber) {
+    if (score > 1) {
+      score--;
+      document.querySelector('.score').textContent = score;
+      // document.querySelector('.message').textContent =
+      //   guess > secretNumber ? '📈Too high!' : '📈Too low!';
+      displayMessage(guess > secretNumber ? '📈Too high!' : '📈Too low!');
+    } else {
+      //document.querySelector('.message').textContent = '💥you loose the game!';
+      displayMessage('💥you loose the game!');
+    }
+  } //when guess to high
+  // else if (guess > secretNumber) {
+  //   if (score > 1) {
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //     document.querySelector('.message').textContent = '📈Too high!';
+  //   } else {
+  //     document.querySelector('.message').textContent = '💥you loose the game!';
+  //   }
+
+  // } //when guess to low
+  // else if (guess < secretNumber) {
+  //   if (score > 1) {
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //     document.querySelector('.message').textContent = '📈Too low!';
+  //   } else {
+  //     document.querySelector('.message').textContent = '💥you loose the game!';
+  //   }
+  // }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
   let score = 20;
   secretNumber = Math.trunc(Math.random() * 20 + 1);
-  document.querySelector('.message').textContent = 'Start guessing...';
+  //document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.score').textContent = score;
   document.querySelector('.guess').value = '';
   document.querySelector('.number').textContent = '?';
